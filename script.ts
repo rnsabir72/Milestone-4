3c598084743a5040d4d373dc0b7a280c415fcf42
rnsabir72 <159696458+rnsabir72@users.noreply.github.com>
2024-12-21 10:57:08 +0500

    Initial commit

---
+++ script.ts
@@ -0,0 +1,42 @@
+// Getting form
+const form= document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplayElement=document.getElementById('resume-display') as HTMLDivElement;
+
+// Handling Form Submission
+
+form.addEventListener('submit', (event: Event) => {
+event.preventDefault();   // Prevent Page Reload
+
+//Taking all input values
+const name=(document.getElementById('name') as HTMLInputElement).value
+const email=(document.getElementById('email') as HTMLInputElement).value
+const phone=(document.getElementById('phone') as HTMLInputElement).value
+const education=(document.getElementById('education') as HTMLInputElement).value
+const experience=(document.getElementById('experience') as HTMLInputElement).value
+const skills=(document.getElementById('skills') as HTMLInputElement).value
+
+//Rsume Generation Dynamically
+
+const resumeHTML= ` 
+<h2><b>Editable Resume</b></h2> 
+<h3>Personal Information</h3> 
+<p><b>Name:</b><span contenteditable="true">${name}</span></p>
+<p><b>Email:</b><span contenteditable="true">${email}</span></p>
+<p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
+
+<h3>Education</h3> 
+<p contenteditable="true">${education}</p>
+
+<h3>Experience</h3> 
+<p contenteditable="true">${experience}</p>
+
+<h3>Skills</h3> 
+<p contenteditable="true">${skills}</p>
+`;
+
+// Displaying Generated Resume
+
+if (resumeDisplayElement){
+resumeDisplayElement.innerHTML=resumeHTML;
+} else {console.error('The Resume Display Element Not Found.')}
+})
